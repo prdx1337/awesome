d8a3fe602b9373e8b2961346db13333e0020d85d
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-09-24 14:55:15 +0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const style = {
   container: `max-w-screen-lg mx-auto pt-12`,
   h1: `dark:text-white md:text-4xl text-2xl font-bold text-gray-900 text-center`,
   wrap: `flex flex-wrap gap-4 px-4 py-6 text-gray-900 dark:text-white`,
-  flex: `md:basis-[40%] basis-full flex flex-1 rounded-lg border dark:border-[#4B5563] items-center justify-center flex-col py-4`,
+  flex: `md:basis-[40%] basis-full flex flex-1 rounded-lg border dark:border-[#4B5563] items-center justify-center flex-col p-4`,
   button: `text-white mt-4 cursor-pointer inline-flex items-center py-2 px-4 text-sm font-medium text-center bg-[#0A1929] hover:text-[#64FFDA] rounded-2xl hover:scale-105 hover:rounded-none duration-200 ease-in-out`,
   footer: `flex justify-center items-center gap-4 pb-4 text-slate-900 dark:text-white`,
   icon: `w-6 h-6 cursor-pointer hover:text-[#64FFDA] duration-200 ease-in-out`,
