35802275376e783deb0b87caad924c603402cbd0
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-09-24 20:46:11 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const style = {
   container: `max-w-screen-lg mx-auto py-12`,
   wrapper: `flex flex-wrap gap-4 h-[600px] relative`,
   flexOne: `md:basis-[45%] basis-full flex justify-center items-center relative`,
-  txthero: `px-4 z-10 text-gray-black dark:text-white`,
+  txthero: `px-4 z-10 text-gray-black dark:text-white overflow-hidden`,
   motion: `font-black text-6xl md:text-8xl cursor-pointer txtshadow tracking-tight leading-none`,
   paragraph: `text-base mt-4 dark:text-[#64FFDA]`,
   flexTwo: `md:basis-[45%] basis-full hidden md:block mx-auto`,
@@ -26,50 +26,59 @@ function Hero() {
     <div className={style.container}>
       <div className={style.wrapper}>
         <div className={style.flexOne}>
-          <motion.div
-            initial={{
-              x: -100,
-              opacity: 0,
-              scale: 0.8,
-            }}
-            animate={{
-              x: 0,
-              opacity: 1,
-              scale: 1,
-            }}
-            transition={{
-              duration: 1.5,
-            }}
-            viewport={{ once: true }}
-            className={style.txthero}
-          >
-            <h1 className={style.motion}>Luther Sedanto</h1>
-            <p className={style.paragraph}>
+          <div className={style.txthero}>
+            <motion.h1
+              initial={{
+                y: 200,
+                skewX: 10,
+              }}
+              animate={{
+                y: 0,
+                skewX: 0,
+              }}
+              transition={{
+                duration: 1,
+              }}
+              viewport={{ once: true }}
+              className={style.motion}
+            >
+              Luther Sedanto
+            </motion.h1>
+            <motion.p
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{
+                duration: 1.3,
+              }}
+              viewport={{ once: true }}
+              className={style.paragraph}
+            >
               <span>{text}</span>
               <Cursor cursorColor="#4B5563" />
-            </p>
-          </motion.div>
+            </motion.p>
+          </div>
           <BackgroundSVG />
         </div>
         <motion.div
           initial={{
-            x: 100,
             opacity: 0,
-            scale: 0.8,
           }}
           animate={{
-            x: 0,
             opacity: 1,
-            scale: 1,
           }}
           transition={{
-            duration: 1.5,
+            duration: 1,
           }}
           viewport={{ once: true }}
           className={style.flexTwo}
         >
           <Spline scene="https://prod.spline.design/I02DuoS6NB58TCc0/scene.splinecode" />
         </motion.div>
+
         <p className="absolute bottom-0 right-0 hidden md:block text-gray-400 text-center">
           Press E, S, C, R on keyboard.
         </p>
