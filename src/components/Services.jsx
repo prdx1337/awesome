df5e5784f77b3dce6f50859d0e9f69ddb51c4139
Luther Sedanto <109575711+muhay007@users.noreply.github.com>
2022-09-25 17:48:34 +0800

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -23,7 +23,7 @@ function FadeInWhenVisible({ children }) {
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      transition={{ duration: 0.5, delay: 0.5 }}
+      transition={{ duration: 0.3, delay: 0.5 }}
       variants={{
         visible: { opacity: 1, scale: 1 },
         hidden: { opacity: 0, scale: 0 },
